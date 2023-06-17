fd1621e4eb1287a466632f09055b6e2f909a5243
BlueGlaucus <pomp.bathos0r@icloud.com>
2023-06-16 23:36:22 -0700

    reconfigured api key

--- src/app/api/newPrompt/route.js
+++ src/app/api/newPrompt/route.js
@@ -41,41 +41,6 @@ export async function POST(request) {
 }
 
 
-  // if (topic.trim().length < 3) {
-  //   res.status(400).json({
-  //     error: {
-  //       message: "Please enter a valid topic",
-  //     }
-  //   });
-  //   return;
-  // }
-  // if(session){
-  //   try {
-  //     const completion = await openai.createCompletion({
-  //       model: "text-davinci-003",
-  //       prompt: generatePrompt(topic, type, side),
-  //       temperature: 0.6,
-  //       max_tokens: 1000
-  //     });
-  
-  //     // res.status(200).json({ result: completion.data.choices[0].text });
-  //     return NextResponse.json({result: completion.data.choices[0].text})
-  //   } catch(error) {
-  //     // // Consider adjusting the error handling logic for your use case
-  //     // if (error.response) {
-  //     //   console.error(error.response.status, error.response.data);
-  //     //   res.status(error.response.status).json(error.response.data);
-  //     // } else {
-  //     //   console.error(`Error with OpenAI API request: ${error.message}`);
-  //     //   res.status(500).json({
-  //     //     error: {
-  //     //       message: 'An error occurred during your request.',
-  //     //     }
-  //     //   });
-  //     // }
-  //   }
-  // }
-
 
 
 
