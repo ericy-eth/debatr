a7c92643617ae2d0c5142da77eb279d35fe9cc61
BlueGlaucus <pomp.bathos0r@icloud.com>
2023-06-16 23:39:51 -0700

    removed response(200)

--- src/app/api/newPrompt/route.js
+++ src/app/api/newPrompt/route.js
@@ -23,20 +23,21 @@ export async function POST(request) {
       prompt: generatePrompt(topic, type, side),
       temperature: 0.6,
     });
-    NextResponse.status(200).json({ result: completion.data.choices[0].text });
+    NextResponse.json({ result: completion.data.choices[0].text });
   } catch(error) {
     // Consider adjusting the error handling logic for your use case
-    if (error.response) {
-      console.error(error.response.status, error.response.data);
-      NextResponse.status(error.response.status).json(error.response.data);
-    } else {
-      console.error(`Error with OpenAI API request: ${error.message}`);
-      NextResponse.status(500).json({
-        error: {
-          message: 'An error occurred during your request.',
-        }
-      });
-    }
+    // if (error.response) {
+    //   console.error(error.response.status, error.response.data);
+    //   NextResponse.status(error.response.status).json(error.response.data);
+    // } else {
+    //   console.error(`Error with OpenAI API request: ${error.message}`);
+    //   NextResponse.status(500).json({
+    //     error: {
+    //       message: 'An error occurred during your request.',
+    //     }
+    //   });
+    // }
+    console.log(error);
   }
 }
 
