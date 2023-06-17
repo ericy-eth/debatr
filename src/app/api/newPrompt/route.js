74046906c0b797857134bb6091455b515f22f228
BlueGlaucus <pomp.bathos0r@icloud.com>
2023-06-16 23:01:55 -0700

    testing newPrompt response

--- src/app/api/newPrompt/route.js
+++ src/app/api/newPrompt/route.js
@@ -26,32 +26,35 @@ export async function POST(request) {
   //   });
   //   return;
   // }
-  if(session){
-    try {
-      const completion = await openai.createCompletion({
-        model: "text-davinci-003",
-        prompt: generatePrompt(topic, type, side),
-        temperature: 0.6,
-        max_tokens: 1000
-      });
+  // if(session){
+  //   try {
+  //     const completion = await openai.createCompletion({
+  //       model: "text-davinci-003",
+  //       prompt: generatePrompt(topic, type, side),
+  //       temperature: 0.6,
+  //       max_tokens: 1000
+  //     });
   
-      // res.status(200).json({ result: completion.data.choices[0].text });
-      return NextResponse.json({result: completion.data.choices[0].text})
-    } catch(error) {
-      // // Consider adjusting the error handling logic for your use case
-      // if (error.response) {
-      //   console.error(error.response.status, error.response.data);
-      //   res.status(error.response.status).json(error.response.data);
-      // } else {
-      //   console.error(`Error with OpenAI API request: ${error.message}`);
-      //   res.status(500).json({
-      //     error: {
-      //       message: 'An error occurred during your request.',
-      //     }
-      //   });
-      // }
-    }
-  }
+  //     // res.status(200).json({ result: completion.data.choices[0].text });
+  //     return NextResponse.json({result: completion.data.choices[0].text})
+  //   } catch(error) {
+  //     // // Consider adjusting the error handling logic for your use case
+  //     // if (error.response) {
+  //     //   console.error(error.response.status, error.response.data);
+  //     //   res.status(error.response.status).json(error.response.data);
+  //     // } else {
+  //     //   console.error(`Error with OpenAI API request: ${error.message}`);
+  //     //   res.status(500).json({
+  //     //     error: {
+  //     //       message: 'An error occurred during your request.',
+  //     //     }
+  //     //   });
+  //     // }
+  //   }
+  // }
+
+    return NextResponse.json({result: "testing testing testing"})
+
 
 }
 
