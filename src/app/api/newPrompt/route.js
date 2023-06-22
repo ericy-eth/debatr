8e820eff47687e8ccaba62f00b15a8897b7622a8
BlueGlaucus <pomp.bathos0r@icloud.com>
2023-06-22 01:22:22 -0700

    newPrompt

--- src/app/api/newPrompt/route.js
+++ src/app/api/newPrompt/route.js
@@ -29,8 +29,9 @@ export async function POST(request) {
       model: "text-davinci-003",
       prompt: generatePrompt(topic, type, side),
       temperature: 0.6,
+      max_tokens: 3000
     });
-    NextResponse.json({ result: completion.data.choices[0].text });
+    return NextResponse.json({ result: completion.data.choices[0].text });
   } catch(error) {
     // Consider adjusting the error handling logic for your use case
     if (error.response) {
--- src/app/home/page.js
+++ src/app/home/page.js
@@ -7,15 +7,25 @@ import { GET } from "../api/auth/[...nextauth]/route";
 import {redirect} from "next/navigation"
 export default async function Home() {
   const res = await getServerSession(GET);
+  let data =  JSON.parse(JSON.stringify(res, null, 2))
+
+  let userSpeeches = null
+
   if(!res){
     redirect('/signup')
+  }else{
+    console.log(data, " data");
+    const resSpeeches = await fetchAllSpeeches(data.user.email)
+
+    if(resSpeeches!=null){
+      userSpeeches = await resSpeeches.json()
+      userSpeeches = userSpeeches.userDocuments
+
+    }
+    // console.log("User speeches ", userSpeeches);
+
   }
-  let data =  JSON.parse(JSON.stringify(res, null, 2))
-  console.log(data, " data");
-  const resSpeeches = await fetchAllSpeeches(data.user.email)
-  let userSpeeches = await resSpeeches.json()
-  userSpeeches = userSpeeches.userDocuments
-  console.log("User speeches ", userSpeeches);
+
     return (
       <>
         <HomeClient props={userSpeeches} session={data}></HomeClient>
